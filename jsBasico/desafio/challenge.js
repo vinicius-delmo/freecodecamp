e9942efaef95eaf67f1ce2d3a3d373c3e8a7a352
Vinícius Delmo <124913845+vinicius-delmo@users.noreply.github.com>
2023-02-22 17:50:42 -0300

    refatorando

--- jsBasico/desafio/challenge.js
+++ jsBasico/desafio/challenge.js
@@ -2,15 +2,17 @@ import { pokemons } from "./data.js";
 import { pokemonTypesInPtBR } from "./data.js";
 
 //Function que procura um pokemon por nome:
-const searchPokemon = (arrPokemon, name) =>
-  arrPokemon.find((pokemons) => pokemons.name === name);
+
+const searchPokemon = (pokemons, name) =>
+  pokemons.find((pokemon) => pokemon.name === name);
 
 const pokemonData = searchPokemon(pokemons, "pidgeot");
 console.log(pokemonData);
+
 //Listar todos os pokemons de um mesmo tipo
 
-const filterPokemons = (pokemonArray, type) =>
-  pokemonArray.filter((pokemons) => pokemons.type.includes(type));
+const filterPokemons = (pokemons, type) =>
+  pokemons.filter((pokemon) => pokemon.type.includes(type));
 
 const sameTypeOfPokemons = filterPokemons(pokemons, "bug");
 console.log(sameTypeOfPokemons);
@@ -18,8 +20,9 @@ console.log(sameTypeOfPokemons);
 // Alterar o nome dos tipos dos pokemons de inglês para português, ex.: Fire => Fogo
 
 const pokemonsInPTBR = pokemons.map((pokemons) => {
-  const changeTypesToPtBR = pokemons.type.map((type) => pokemonTypesInPtBR[type]);
+  const changeTypesToPtBR = pokemons.type.map(
+    (type) => pokemonTypesInPtBR[type]
+  );
   return { ...pokemons, type: changeTypesToPtBR };
 });
-
 console.log(pokemonsInPTBR);
